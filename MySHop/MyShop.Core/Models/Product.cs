ac2c5c1954b2dcaf87e657d225ad12113eeb0659
KevinGaynor <kevin.gaynor@live.co.uk>
2020-06-04 00:27:29 +0100

    Product Manager Pages Built and Tested

--- MySHop/MyShop.Core/Models/Product.cs
+++ MySHop/MyShop.Core/Models/Product.cs
@@ -14,7 +14,7 @@ namespace MyShop.Core.Models
 
         [StringLength(20)]
         [DisplayName("Product Name")]
-        public string Name { get; set }
+        public string Name { get; set; }
 
         public string Description { get; set; }
 
